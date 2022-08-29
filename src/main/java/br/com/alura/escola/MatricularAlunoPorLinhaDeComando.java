4e55e4f8661778c9ca74224b4e826554efe182c8
Hiago Petris <hiago.petris@gmail.com>
2022-08-29 08:53:59 -0300

    Context

--- src/main/java/br/com/alura/escola/MatricularAlunoPorLinhaDeComando.java
+++ src/main/java/br/com/alura/escola/MatricularAlunoPorLinhaDeComando.java
@@ -1,10 +1,13 @@
 package br.com.alura.escola;
 
-import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAluno;
-import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAlunoDto;
-import br.com.alura.escola.dominio.PublicadorDeEventos;
-import br.com.alura.escola.dominio.aluno.LogDeAlunoMatriculado;
-import br.com.alura.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAluno;
+import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAlunoDto;
+import br.com.alura.escola.academico.dominio.aluno.LogDeAlunoMatriculado;
+import br.com.alura.escola.academico.infra.aluno.RepositorioDeAlunosEmMemoria;
+import br.com.alura.escola.gamificacao.aplicacao.GeraSeloAlunoNovato;
+import br.com.alura.escola.gamificacao.infra.selo.RepositorioDeSelosEmMemoria;
+import br.com.alura.escola.shared.dominio.CPF;
+import br.com.alura.escola.shared.dominio.evento.PublicadorDeEventos;
 
 public class MatricularAlunoPorLinhaDeComando {
     public static void main(String[] args) {
@@ -14,12 +17,19 @@ public class MatricularAlunoPorLinhaDeComando {
 
         PublicadorDeEventos publicador = new PublicadorDeEventos();
         publicador.adicionar(new LogDeAlunoMatriculado());
+        RepositorioDeSelosEmMemoria repositorioDeSelosEmMemoria = new RepositorioDeSelosEmMemoria();
+        publicador.adicionar(new GeraSeloAlunoNovato(repositorioDeSelosEmMemoria));
+
         MatricularAluno matricularAluno = new MatricularAluno(new RepositorioDeAlunosEmMemoria(), publicador);
 
         // Command design pattern
         matricularAluno.executa(
                 new MatricularAlunoDto(nome, cpf, email)
         );
+
+        CPF cpfObj = new CPF(cpf);
+
+        System.out.println(repositorioDeSelosEmMemoria.selosDoAlunoDeCPF(cpfObj).get(0).getNome());
 
     }
 }
